1414690777ce4dcd6c4c5fc5c27e701a4c2ea797
Heropaster <heropasta@mail.ru>
2023-07-11 14:58:08 +0700

    navigation logic

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./About.css"
 const About = () => {
+    const [isActive, setActive] = React.useState(false)
+    function handleToggle() {
+        setActive(!isActive)
+    }
     return (
         <nav className="navigation">
-                <div className="navigation__item">
+                <div className={isActive ? "navigation__item active": "navigation__item"} onClick={handleToggle}>
                     <span className='navigation__line navigation__line_first'></span>
                     <span className='navigation__line navigation__line_second'></span>
                     <span className='navigation__line navigation__line_third'></span>
                 </div>
-            <div className="aboutBg"></div>
+            <div className={isActive ? "aboutBg active": "aboutBg"}></div>
         </nav>
     )
 }
