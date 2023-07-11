fb83e24efee7f5048b6bd9e271b42d2e42721b40
Heropaster <heropasta@mail.ru>
2023-07-11 15:26:42 +0700

    Уменьшил тернарный оператор для scss класса active

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,12 +7,12 @@ const About = () => {
     }
     return (
         <nav className="navigation">
-                <div className={isActive ? "navigation__item active": "navigation__item"} onClick={handleToggle}>
+                <div className={`navigation__item ${isActive ? "active": null}`} onClick={handleToggle}>
                     <span className='navigation__line navigation__line_first'></span>
                     <span className='navigation__line navigation__line_second'></span>
                     <span className='navigation__line navigation__line_third'></span>
                 </div>
-            <div className={isActive ? "aboutBg active": "aboutBg"}></div>
+            <div className={`aboutBg ${isActive ? "active": null}`}></div>
         </nav>
     )
 }
