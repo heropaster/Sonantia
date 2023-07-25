9fb327b5151093671b402e3ec2168f23dcf47d87
Heropaster <heropasta@mail.ru>
2023-07-25 15:31:45 +0700

    accordion realization

--- src/components/About/Accordion/Accordions.tsx
+++ src/components/About/Accordion/Accordions.tsx
@@ -2,14 +2,21 @@ import React from 'react'
 
 import './Accordions.css'
 const Accordions = () => {
+    function showContent(target: HTMLElement) {
+        target.classList.toggle('active');
+        let targetContent = target.nextElementSibling!
+        targetContent.classList.toggle('showed')
+    }
     return (
         <div className="accordion-container">
             <div className="accordion__item">
-                <div className="accordion__item__header">
-                    Что такое Sonantia?
+                <div className="accordion__item__header" onClick={(event: any) => {
+                    showContent(event.target)
+                }}>
+                    Что такое <span className="blue">Sonantia</span> ?
                 </div>
                 <div className="accordion__item__body">
-                    Sonantia - приложение, которое воспроизводит слова на английском языке и проверяет ваше понимание языка на звук.
+                    <span className="blue">Sonantia</span> - приложение, которое воспроизводит слова на английском языке и проверяет ваше понимание языка на звук.
                 </div>
             </div>  
         </div>
