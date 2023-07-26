acf04a74694149a8e491a5c256b21911a165e7b8
Heropaster <heropasta@mail.ru>
2023-07-26 16:24:19 +0700

    fix bug when click on blue

--- src/components/About/Accordion/Accordions.tsx
+++ src/components/About/Accordion/Accordions.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import './Accordions.css'
 const Accordions = () => {
     function showContent(target: HTMLElement) {
+        if (target.classList.contains('blue')) return
         target.classList.toggle('active');
         let targetContent = target.nextElementSibling!
         targetContent.classList.toggle('showed')
@@ -11,6 +12,10 @@ const Accordions = () => {
         <div className="accordion-container">
             <div className="accordion__item">
                 <div className="accordion__item__header" onClick={(event: any) => {
+                    if (event.target.classList.contains('blue')) {
+                        console.log(event.target.parentNode)
+                        showContent(event.target.parentNode)
+                    }
                     showContent(event.target)
                 }}>
                     Что такое <span className="blue">Sonantia</span> ?
